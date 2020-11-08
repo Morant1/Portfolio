0299fdd97c50b8a3dcfafc5bc87fa8d37949fd22
Morant1 <moran8865@walla.com>
2020-11-08 16:03:33 +0200

    Updates

---
+++ precache-manifest.aa621b833edd7ba34a1e164f00e77505.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "036c161d026b39aa1d627adc9e97c737",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "b2ba05d2bc4b0ab7f4eb",
+    "url": "/Portfolio/static/css/main.b0a06481.chunk.css"
+  },
+  {
+    "revision": "8f6bc78184e380d649cf",
+    "url": "/Portfolio/static/js/2.f5204706.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.f5204706.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ba05d2bc4b0ab7f4eb",
+    "url": "/Portfolio/static/js/main.0bd32803.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "b20ff458bc0dc48c039c68a3dc96173c",
+    "url": "/Portfolio/static/media/Caveat-Regular.b20ff458.ttf"
+  },
+  {
+    "revision": "fddc5fa6efb91e49befb6ad849da9b0f",
+    "url": "/Portfolio/static/media/Livvic-Bold.fddc5fa6.ttf"
+  },
+  {
+    "revision": "9939ebf90a63343d97a1f13099d07068",
+    "url": "/Portfolio/static/media/Livvic-MediumItalic.9939ebf9.ttf"
+  },
+  {
+    "revision": "fe84dbc6ce317e6216d959cf7baed69c",
+    "url": "/Portfolio/static/media/Livvic-SemiBold.fe84dbc6.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c30969cd947709d998bff3d1183a1b6f",
+    "url": "/Portfolio/static/media/angular.c30969cd.png"
+  },
+  {
+    "revision": "d9bb87a3c5aaebe231ae435efa71f242",
+    "url": "/Portfolio/static/media/angular2.d9bb87a3.png"
+  },
+  {
+    "revision": "39c0334b57e661e44855933e3a820cb0",
+    "url": "/Portfolio/static/media/appsus.39c0334b.png"
+  },
+  {
+    "revision": "1e8f9ddfe9bd8b32e1f4d67a2c2165d2",
+    "url": "/Portfolio/static/media/css.1e8f9ddf.png"
+  },
+  {
+    "revision": "6ecbc922770a47343da5507742a56645",
+    "url": "/Portfolio/static/media/cv.6ecbc922.pdf"
+  },
+  {
+    "revision": "e07dbce239e5cd834e45cb3b72d2691e",
+    "url": "/Portfolio/static/media/eventribe.e07dbce2.png"
+  },
+  {
+    "revision": "a58728919c10c74e25a9b656c478f730",
+    "url": "/Portfolio/static/media/jquery.a5872891.png"
+  },
+  {
+    "revision": "c647884d2940f119b665a453f9c2c8f3",
+    "url": "/Portfolio/static/media/js.c647884d.png"
+  },
+  {
+    "revision": "d24a5e659bc5acd47375f87c37d95e69",
+    "url": "/Portfolio/static/media/logo.d24a5e65.jpg"
+  },
+  {
+    "revision": "43516fb179d764125cba39d4f0ac7240",
+    "url": "/Portfolio/static/media/meme.43516fb1.png"
+  },
+  {
+    "revision": "0fbd04cba8e061b55e9da307a880f8f3",
+    "url": "/Portfolio/static/media/mongo.0fbd04cb.png"
+  },
+  {
+    "revision": "4c6c0929cc58f241847a744b2de75e2f",
+    "url": "/Portfolio/static/media/nodejs.4c6c0929.png"
+  },
+  {
+    "revision": "cab43a43a308de3fea7f466e833c8d89",
+    "url": "/Portfolio/static/media/python.cab43a43.png"
+  },
+  {
+    "revision": "f6ee6c98a8671e6f9e9e7a8d4d44947b",
+    "url": "/Portfolio/static/media/react.f6ee6c98.png"
+  },
+  {
+    "revision": "6597cc35589236a430e20f4d2adf09a6",
+    "url": "/Portfolio/static/media/sass.6597cc35.png"
+  },
+  {
+    "revision": "969a19922798f8f62ca1fc319c898d1c",
+    "url": "/Portfolio/static/media/splash.969a1992.png"
+  },
+  {
+    "revision": "8eb01c0aa35a05bb8480ff3335dddd01",
+    "url": "/Portfolio/static/media/sql.8eb01c0a.png"
+  },
+  {
+    "revision": "2c89899b7e390a435681ac5e96ad2de2",
+    "url": "/Portfolio/static/media/vue.2c89899b.png"
+  },
+  {
+    "revision": "c95d1245bcef6e5f89042057925aff9d",
+    "url": "/Portfolio/static/media/vue2.c95d1245.png"
+  }
+]);
